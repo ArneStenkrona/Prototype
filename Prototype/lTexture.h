1756eb2c0448a02991416af03cab115d437ac230
ArneStenkrona <arne.stenkrona@me.com>
2017-12-15 00:04:17 +0100

    Added wrapper class for SDL textures

---
+++ Prototype/lTexture.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <string>
+class LTexture
+{
+public:
+    //Initializes variables
+    LTexture();
+
+    //Deallocates memory
+    ~LTexture();
+
+    //Loads image at specified path
+    bool loadFromFile(std::string path);
+
+    //Deallocates texture
+    void free();
+
+    //Renders texture at given point
+    void render(int x, int y);
+
+    //Renders texture at given point with rotation and flip
+    void renderEx(int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+    //Renders the tile in the texture given by the tileQuad
+    void renderTile(int x, int y, SDL_Rect *tileQuad);
+
+    //Renders the tile in the texture given by the tileQuad with roation and flip
+    void renderTileEx(int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE, SDL_Rect *tileQuad = NULL);
+
+    //Renders texture with center at given point
+    void renderCenter(int x, int y);
+
+    //Gets image dimensions
+    int getWidth();
+    int getHeight();
+
+private:
+    //The actual hardware texture
+    SDL_Texture* mTexture;
+
+    //Image dimensions
+    int mWidth;
+    int mHeight;
+};
